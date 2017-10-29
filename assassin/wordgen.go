ae6fcea9a7e0d6e768bbb0ae44d165644c0baa27
Joshua Ringer <joshringer@users.noreply.github.com>
2017-10-29 23:06:06 +0800

    Add basic WordGenerator that operates off of a word list.

--- assassin/wordgen.go
+++ assassin/wordgen.go
@@ -1,9 +1,56 @@
 package assassin
 
+import (
+	"bufio"
+	"io"
+	"math/rand"
+)
+
 /*
 WordGenerator interface for generating player KillWords.
 	Gen() generates and returns a single word
 */
 type WordGenerator interface {
 	Gen() string
+}
+
+/*
+ListPickWordGenerator is a WordGenerator that selects words from a pre-provided
+list of words.
+*/
+type ListPickWordGenerator struct {
+	wordList []string
+	current  int
+}
+
+// NewListPickWordGenerator creates a ListPickWordGenerator from a given wordList.
+func NewListPickWordGenerator(words []string) *ListPickWordGenerator {
+	var g = &ListPickWordGenerator{make([]string, 0), 0}
+	for _, i := range rand.Perm(len(words)) {
+		g.wordList = append(g.wordList, words[i])
+	}
+	return g
+}
+
+/*
+ListPickWordGeneratorFromReader creates a ListPickWordGenerator, reading the
+contents of the provided r to construct the word list.
+*/
+func ListPickWordGeneratorFromReader(r io.Reader) (*ListPickWordGenerator, error) {
+	var (
+		s = bufio.NewScanner(r)
+		w = make([]string, 0)
+	)
+	s.Split(bufio.ScanWords)
+	for s.Scan() {
+		w = append(w, s.Text())
+	}
+	return NewListPickWordGenerator(w), s.Err()
+}
+
+// Gen picks the next word from the list.
+func (g *ListPickWordGenerator) Gen() string {
+	var w = g.wordList[g.current]
+	g.current = (g.current + 1) % len(g.wordList)
+	return w
 }
--- assassin/wordgen_test.go
+++ assassin/wordgen_test.go
@@ -1,5 +1,10 @@
 package assassin
 
+import (
+	"strings"
+	"testing"
+)
+
 type dummyWordGen struct {
 	b byte
 }
@@ -8,4 +13,20 @@ func (d *dummyWordGen) Gen() string {
 	var c = string(d.b)
 	d.b++
 	return c + c + c + c
+}
+
+func TestListPickWordGen(t *testing.T) {
+	var (
+		wl  = strings.NewReader("two\n   one   three")
+		exp = []string{"one", "two", "three", "one"}
+	)
+	var g, err = ListPickWordGeneratorFromReader(wl)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i := 0; i < 4; i++ {
+		if w := g.Gen(); w != exp[i] {
+			t.Error("Unexpected Gen order for iteration", i, w, "!=", exp[i])
+		}
+	}
 }
